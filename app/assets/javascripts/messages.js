b36282f52196280254673658daf0d697dff841f3
Nagatakidani <ryuseibasketball2@gmail.com>
2020-03-15 17:13:57 +0900

    add Automatic Updating 02

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -82,7 +82,6 @@ $(function(){
 
   var reloadMessages = function() {
     var last_message_id = $('.message:last').data("message-id");
-    console.log(last_message_id)
     $.ajax({
   url: "api/messages",
   type: 'get',
@@ -90,7 +89,6 @@ $(function(){
   data: {id: last_message_id}
   })
   .done(function(messages) {
-    console.log(messages)
     if (messages.length !== 0) {
       var insertHTML = '';
       $.each(messages, function(i, message) {
@@ -105,7 +103,7 @@ $(function(){
   });
 };
 if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-  setInterval(reloadMessages, 1500);
+  setInterval(reloadMessages, 500);
 }
 
 });
